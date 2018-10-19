9956c88da9dffb2593d1f4e8e399736b42e24e00
Norman Walsh <ndw@nwalsh.com>
2018-10-18 21:07:57 -0500

    Added tests

---
+++ src/test/scala/com/xmlcalabash/test/TemplateTestSpec.scala
@@ -0,0 +1,7 @@
+package com.xmlcalabash.test
+
+import com.xmlcalabash.util.XProcTestSpec
+
+class TemplateTestSpec extends XProcTestSpec {
+  runtests("Run the template tests", "src/test/resources/tests")
+}
